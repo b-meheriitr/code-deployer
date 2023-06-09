491cc213673fec3b70536ab2f0466d44492c65a9
bhubaneshwar.meher <bhubaneshwar.meher@ril.com>
2023-06-09 16:39:53 +0530

    Add zip flag to deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,9 +1,15 @@
 import archiver from 'archiver'
 import axios from 'axios'
 import FormData from 'form-data'
+import fsSync, {promises as fs} from 'fs'
 import minimist from 'minimist'
+import moment from 'moment'
+import path from 'path'
 
 const nodeModulesToZip = minimist(process.argv.slice(2)).ibd ? [] : ['node_modules/**']
+
+const onlyZip = !!minimist(process.argv.slice(2)).zip
+const zipStorageDirectory = 'dist/zips'
 
 const CONFIG = {
 	// DEPLOYMENT_API_URL: 'http://10.21.54.93:3500/api/deploy',
@@ -12,6 +18,7 @@ const CONFIG = {
 	APP_NAME: 'code-deployer',
 	DEPLOYMENT_IGNORE_DELETE: [
 		'logs/**',
+		'data/**',
 		...nodeModulesToZip,
 	],
 	FILES_TO_ZIP: [
@@ -59,23 +66,46 @@ const buildFormData = (archive, ignoreDelete) => {
 
 const formData = buildFormData(createZipArchiveStream(), CONFIG.DEPLOYMENT_IGNORE_DELETE)
 
-console.log('Deploying to host ...')
-axios({
-	method: 'POST',
-	url: CONFIG.DEPLOYMENT_API_URL,
-	data: formData,
-	headers: {
-		...formData.getHeaders(),
-		'app-name': CONFIG.APP_NAME,
-	},
-})
-	.then(
-		response => console.log(response.status, JSON.stringify(response.data, null, 4)),
-		(err) => {
-			if (err.response) {
-				console.log('Api ERROR', err.response.status, JSON.stringify(err.response.data, null, 4))
-			} else {
-				console.log(err.message)
-			}
+if (onlyZip) {
+	new Promise(async (resolve, reject) => {
+		const outFile = path.join(
+			zipStorageDirectory,
+			`${CONFIG.APP_NAME}-${moment().format('DD-MM-YYYY_HH-mm-ss')}.zip`,
+		)
+		await fs.mkdir(path.dirname(outFile), {recursive: true})
+
+		const outputStream = fsSync.createWriteStream(outFile)
+		const archiver = createZipArchiveStream()
+
+		archiver.on('error', (err) => reject(err))
+		outputStream.on('error', (err) => reject(err))
+		outputStream.on('close', () => resolve(outFile))
+
+		archiver.pipe(outputStream)
+	})
+		.then((out) => console.log('done zipping', out))
+} else {
+	console.log('Deploying to host ...')
+	const startTime = new Date().getTime()
+
+	axios({
+		method: 'POST',
+		url: CONFIG.DEPLOYMENT_API_URL,
+		data: formData,
+		headers: {
+			...formData.getHeaders(),
+			'app-name': CONFIG.APP_NAME,
 		},
-	)
+	})
+		.then(
+			response => console.log(response.status, JSON.stringify(response.data, null, 4)),
+			(err) => {
+				if (err.response) {
+					console.log('Api ERROR', err.response.status, JSON.stringify(err.response.data, null, 4))
+				} else {
+					console.log(err.message)
+				}
+			},
+		)
+		.finally(() => console.log('time: ', (new Date().getTime() - startTime) / 1000))
+}
